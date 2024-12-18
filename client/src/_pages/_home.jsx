2ca5e4c01c025f4e45e466fb69c387f3905caebe
Jawad <jawadal829@gmail.com>
2024-12-18 11:43:50 +0500

    hero designed

--- client/src/_pages/_home.jsx
+++ client/src/_pages/_home.jsx
@@ -1,23 +1,57 @@
 import React from 'react'
 import '../_styles/_home.scss'
-import { CgArrowLongDown } from 'react-icons/cg'
+import { CgArrowLongDown, CgArrowLongRight } from 'react-icons/cg'
 import Header from '../_components/_header'
 
 const Home = () => {
     return (
-        <div className='home-container flex'>
+        <div className='home-container flex col'>
             <Header />
             <div className="top-container flex">
                 <div className="left-intro flex col">
                     <h1>Welcome to the Smart Way to Plan Your Career.</h1>
                 </div>
                 <div className="bottom-container flex">
-                    
                 <div className="arrow-bottom flex">
                     <div className="icon flex">
                         <CgArrowLongDown />
                     </div>
                 </div>
+                </div>
+            </div>
+            <div className="how-it-works flex col">
+                <h1>Understand the flow</h1>
+                <div className="cards flex">
+                    <div className="card flex col">
+                        <h2>create your profile</h2>
+                        <p>Fill out a simple form to share your goals, skills, and interests and get logged in</p>
+                        <button className="btn flex">
+                            Let's Start
+                            <div className="icon flex">
+                                <CgArrowLongRight />
+                            </div>
+                        </button>
+                    </div>
+                    <div className="card flex col">
+                        <h2>Receive Personalized Insights</h2>
+                        <p>Our AI analyzes your input to recommend personalized career paths.</p>
+                        <button className="btn flex">
+                            Let's Start
+                            <div className="icon flex">
+                                <CgArrowLongRight />
+                            </div>
+                        </button>
+                    </div>
+                    <div className="card flex col">
+                        <h2>Get Mentored</h2>
+                        <p>Engage with your AI mentor or connect with industry professionals for advice.</p>
+                        <button className="btn flex">
+                            Let's Start
+                            <div className="icon flex">
+                                <CgArrowLongRight />
+                            </div>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
